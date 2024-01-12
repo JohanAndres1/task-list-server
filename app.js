4c9700d28ee898765c6b52f2b31531c7247a760d
Johan Sierra <johansierra553@gmail.com>
2024-01-11 19:30:57 -0500

    se cambio la respuesta de la ruta raiz para devolver un unico objeto de tarea codificado en lugar de la raizde la tarea

--- app.js
+++ app.js
@@ -2,21 +2,16 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-// Lista de tareas
-const tasks = [
-  {
-    id: '123456',
-    isCompleted: false,
-    description: 'Walk the dog',
-  }
-]
-
-// Ruta para obtener la lista de tareas
 app.get('/', (req, res) => {
-  res.json(tasks);
+    res.json({
+      id: '123456',
+      isCompleted: false,
+      description: 'Walk the dog'
+    })
 })
 
-// Inicia el servidor
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`)
-})
+const server = app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+module.exports = server;
